ac9e121536137c6faa4e4ca388a2e7755add8681
Tomasz Zbigniew Czaplicki <t.czaplicki@stud.elka.pw.edu.pl>
2023-11-23 13:41:35 +0100

    added LP & solved net12.dat

--- run_console.bat
+++ run_console.bat
@@ -1,3 +1,5 @@
 "..\..\Ampl\ampl.exe" dap.run
+"..\..\Ampl\ampl.exe" dap_lp.run
 "..\..\Ampl\ampl.exe" ddap.run
+"..\..\Ampl\ampl.exe" ddap_lp.run
 pause
---
+++ run_console12.bat
@@ -0,0 +1,5 @@
+"..\..\Ampl\ampl.exe" dap12.run
+"..\..\Ampl\ampl.exe" dap_lp12.run
+"..\..\Ampl\ampl.exe" ddap12.run
+"..\..\Ampl\ampl.exe" ddap_lp12.run
+pause
